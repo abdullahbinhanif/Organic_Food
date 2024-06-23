9c0c01ee8b5969afec209d4d9fe5f594b2f51182
unknown <abdullahabirweb10@gmail.com>
2024-06-23 20:59:19 +0600

    categorymenu

--- src/components/CategoryMenu.jsx
+++ src/components/CategoryMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import FoodData from "../data/FoodData";
-
+import { setCategory } from "../redux/slices/CategorySlice";
+import { useDispatch, useSelector } from "react-redux";
 const CategoryMenu = () => {
   const [categories, setCategories] = useState([]);
 
@@ -16,13 +17,25 @@ const CategoryMenu = () => {
     listUniqueCategories();
   }, []);
 
+  const dispacth = useDispatch();
+  const selectedCategory = useSelector((state) => state.category.category);
   return (
     <div className="ml-6">
       <h3 className="text-xl font-semibold">Find the best food</h3>
       <div className="my-5 flex gap-3 overflow-x-scroll scroll-smooth  lg:overflow-x-hidden">
+        <button
+          onClick={() => dispacth(setCategory("All"))}
+          className="px-3 py-2 bg-purple-500 font-bold rounded-lg hover:bg-purple-600 hover:text-white"
+        >
+          All
+        </button>
         {categories.map((category, index) => {
           return (
-            <button className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-purple-600 hover:text-white">
+            <button
+              onClick={() => dispacth(setCategory(category))}
+              key={index}
+              className="px-3 py-2 bg-gray-200 font-bold rounded-lg hover:bg-purple-600 hover:text-white"
+            >
               {category}
             </button>
           );
